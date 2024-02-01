20fbc00954bd27c4a6e29a84c1438575e782c7c5
ygreee <ygreee0320@naver.com>
2024-02-01 16:31:31 +0900

    Bottom bar 생성

--- app/src/main/java/com/example/prography/MainActivity.kt
+++ app/src/main/java/com/example/prography/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.prography
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.prography.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //toolbar
+        setSupportActionBar(binding.toolbar)
+
+        //bottom navigator
+        binding.bottomMenu.setOnItemSelectedListener {item ->
+            changeFragment(
+                when (item.itemId) {
+                    R.id.home -> HomeFragment()
+                    else -> RandomFragment()
+                }
+            )
+            true
+        }
+    }
+
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.fragment_container,fragment)
+            .commit()
     }
 }
